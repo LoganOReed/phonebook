30d622a72c3a70e478986f3987b4c2bf64d4bd40
Logan Reed <me@loganreed.org>
2023-09-30 05:18:34 -0400

    Added Numbers to Structure

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -5,16 +5,23 @@ import './App.css'
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewName = (e) => {
     e.preventDefault()
 
-    setPersons(persons.concat({name: newName}))
-    console.log(persons)
-    
+    if(persons.some(p => p.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+    }
+    else{
+      setPersons(persons.concat({name: newName, number: newNumber}))
+    }
   }
   return (
     <>
@@ -22,18 +29,21 @@ function App() {
       <h2>Phonebook</h2>
       <form onSubmit={addNewName}>
         <div>
-         name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+         name: <input name="name" value={newName} onChange={e => setNewName(e.target.value)} />
+        </div>
+        <div>
+         number: <input name="number" value={newNumber} onChange={e => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
+
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
       <div>
           {persons.map(person => 
             <p key={person.name}>
-              {person.name}
+              {person.name} :: {person.number}
             </p>
           )}
       </div>
